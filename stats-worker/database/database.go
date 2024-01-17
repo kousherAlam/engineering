6806fccddbe02f01df7f83b4d8aa17cfab68ebcf
Kousher alam <kousheralampranto@gmail.com>
2024-01-18 00:30:18 +0600

    stats-worker/database - added database connection

---
+++ stats-worker/database/database.go
@@ -0,0 +1,46 @@
+package database
+
+// Connects to MongoDB and sets a Stable API version
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Replace the placeholder with your Atlas connection string
+var client *mongo.Client
+
+func GetMongoClient(host, user, password, dbname string) *mongo.Client {
+	if client == nil {
+		return client
+	}
+	var uri = fmt.Sprintf("mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority", user, password, host, dbname)
+	var err error
+
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+	// Create a new client and connect to the server
+	client, err = mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// Send a ping to confirm a successful connection
+	var result bson.M
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Decode(&result); err != nil {
+		panic(err)
+	}
+	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	return client
+}
--- stats-worker/main.go
+++ stats-worker/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/kousheralam/stats-worker/database"
+)
 
 func main() {
 	fmt.Println("Background worker lunched!")
+	database.GetMongoClient("localhost:27017", "user", "password", "users")
+	// client.Database("users").Collection("users").InsertOne(context.Background(), bson.M{"name": "kousher", "age": 22})
 }
